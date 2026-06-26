d66411d205e7243577fde08cfbce32315cccee00
AnanyaGBharadwaj <ananyagbharadwaj@gmail.com>
2026-06-26 18:16:36 +0530

    Fix Render port binding

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+// require('dotenv').config();
+// const app = require('./app');
+// const connectDB = require('./config/db');
+
+// // Connect to Database
+// connectDB();
+
+// const PORT = process.env.PORT || 5001;
+
+// const server = app.listen(PORT, '127.0.0.1', () => {
+//   console.log(`
+//    VendorConnect FINAL running
+//    URL: http://127.0.0.1:${PORT}
+//   `);
+// });
+
+// // Handle unhandled promise rejections
+// process.on('unhandledRejection', (err, promise) => {
+//   console.log(`Error: ${err.message}`);
+//   // Close server & exit process
+//   server.close(() => process.exit(1));
+// });
+
+
 require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/db');
@@ -7,16 +31,15 @@ connectDB();
 
 const PORT = process.env.PORT || 5001;
 
-const server = app.listen(PORT, '127.0.0.1', () => {
+const server = app.listen(PORT, () => {
   console.log(`
    VendorConnect FINAL running
-   URL: http://127.0.0.1:${PORT}
+   URL: http://0.0.0.0:${PORT}
   `);
 });
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on('unhandledRejection', (err) => {
   console.log(`Error: ${err.message}`);
-  // Close server & exit process
   server.close(() => process.exit(1));
 });
